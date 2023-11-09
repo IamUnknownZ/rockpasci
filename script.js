6ea67bb2fb611a222efd77874ea7fd8c821b8c40
IamUnknownZ <kirigato710@gmail.com>
2023-11-09 20:13:28 +0700

    nmnm

--- script.js
+++ script.js
@@ -5,37 +5,24 @@ function getRandomChoice() {
   return choices[randomIndex];
 }
 
-function determineWinner(player1Choice, player2Choice) {
-  if (player1Choice === player2Choice) {
+function determineWinner(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
     return 'It\'s a tie!';
   } else if (
-    (player1Choice === 'rock' && player2Choice === 'scissors') ||
-    (player1Choice === 'paper' && player2Choice === 'rock') ||
-    (player1Choice === 'scissors' && player2Choice === 'paper')
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
   ) {
-    return 'Player 1 wins!';
+    return 'You win!';
   } else {
-    return 'Player 2 wins!';
+    return 'You lose!';
   }
 }
 
-function makeChoice(player, choice) {
-  if (player === 'player1') {
-    const player2ChoiceInput = document.getElementById('player2Choice');
-    player2ChoiceInput.disabled = false;
-    player2ChoiceInput.focus();
-    player2ChoiceInput.value = '';
-    document.getElementById('outcome').textContent = 'Waiting for Player 2...';
-  } else {
-    const player2ChoiceInput = document.getElementById('player2Choice');
-    player2ChoiceInput.disabled = true;
-    const player1Choice = document.getElementById('player1Choice').textContent.toLowerCase();
-    const player2Choice = choice.toLowerCase();
-    document.getElementById('outcome').textContent = determineWinner(player1Choice, player2Choice);
-  }
-}
-
-function getPlayer2Choice() {
-  const player2ChoiceInput = document.getElementById('player2Choice');
-  return player2ChoiceInput.value.trim().toLowerCase();
+function makeChoice(playerChoice) {
+  const computerChoice = getRandomChoice();
+  document.getElementById('computerSelection').textContent = computerChoice;
+  
+  const outcome = determineWinner(playerChoice, computerChoice);
+  document.getElementById('outcome').textContent = outcome;
 }
